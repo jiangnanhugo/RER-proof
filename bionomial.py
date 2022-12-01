ebf0babc1ce831b0e14e185f8fbb29eb42b4f270
Nan Jiang <jiang631@purdue.edu>
2022-11-30 22:08:42 -0500

    Update bionomial.py

--- bionomial.py
+++ bionomial.py
@@ -35,3 +35,21 @@ print(bi(2*L-1,k)-bi(L-2,k))
 # Out[11]: 635701638
 print(sumed)
 # Out[12]: 635653018
+
+#
+# $\sum_{l=1}^L {2*l-2 \choose k-1 }$ 
+# should be less than: 
+# ${2L-1\choose k} - {2L-3\choose k-2}$ 
+#
+
+
+In [28]: sumed=0
+    ...: for l in range(1,L+1):
+    ...:     sumed+=bi(2*l-2, k-2)
+    ...:
+
+In [29]: bi(2*L-1,k-1)-bi(2*L-3,k-2)
+Out[29]: 173307112
+
+In [30]: sumed
+Out[30]: 119603024

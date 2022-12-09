8a67e35941a3bdef5174d03231b800d69835f9b4
Nan Jiang <jiang631@purdue.edu>
2022-12-09 17:02:52 -0500

    Update vector-martrix.py

--- vector-martrix.py
+++ vector-martrix.py
@@ -9,3 +9,13 @@ In [23]: A=np.eye(12);
     ...:
 
 In [24]: np.transpose(A)==A
+
+    
+    
+    
+# upper bound comparison
+ etas,Ls,z_comb=[],[],[];
+     ...: for eta in np.linspace(0.01, 0.99, 99, endpoint=True):
+     ...:     for L in np.linspace(2,100, 98, endpoint=True):
+     ...:         etas.append(eta); Ls.append(L);
+     ...:         z_comb.append((1-eta)**(2*L) - (1-eta)**(2*L-3) + (1-eta)**(L) - (1-eta)**(L-2))

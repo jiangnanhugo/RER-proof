128376d96659c1623bca09e6129fad5dbad28b46
Nan Jiang <jiang631@purdue.edu>
2022-12-11 01:32:37 -0500

    Update bionomial.py

--- bionomial.py
+++ bionomial.py
@@ -25,14 +25,14 @@ print(sumed)
 #
 # $\sum_{l=1}^L {L+l-2 \choose k-1 }$ 
 # should be equal to: 
-# ${2L-1\choose k} - {L-2\choose k}$ 
+# ${2L-1\choose k} - {L-1\choose k}$ 
 #
 sumed=0
 for l in range(1,L+1):
   sumed+=bi(L+l-2, k-1)
 
-print(bi(2*L-1,k)-bi(L-2,k))
-# Out[11]: 635701638
+print(bi(2*L-1,k)-bi(L-1,k))
+# Out[11]: 635653018
 print(sumed)
 # Out[12]: 635653018
 
